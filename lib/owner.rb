e4e1fd873bb2f0919516058e5ea62cd7348347df
Adam Palmer <theadampalmer@gmail.com>
2020-07-22 19:52:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,18 @@
 class Owner
+  attr_reader :species, :name
   
-  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
