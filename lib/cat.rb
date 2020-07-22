76f74740cb512afd105197184afe276f32854ce3
Adam Palmer <theadampalmer@gmail.com>
2020-07-22 21:29:47 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,17 @@
 class Cat
-  
+  attr_accessor :mood, :owner
   attr_reader :name
-  attr_accessor :mood
 
-  def initialize(name)
+  @@all = []
+
+  def initialize(name, owner)
     @name = name
+    @owner = owner
     @mood = "nervous"
+    @@all << self
   end
-end
+
+  def self.all
+    @@all
+  end
+end 
